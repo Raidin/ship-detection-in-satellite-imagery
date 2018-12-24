ef18b0f04579d5bfb70be34b27346f9f4c48c5f9
ContJihunJung <jhjung@contec.kr>
2018-12-24 18:13:13 +0900

    Detection Box Lable 표시 해주는 부분 Opencv, matplotlib 구현 Sample 추가

--- etc/temprary.py
+++ etc/temprary.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
-
+import cv2
 
 def ReadImage(image_path, idx):
     image = Image.open(image_path)
@@ -38,6 +38,29 @@ def NumpyConcatenateExample():
     print b.shape
     print c.shape
     print c
+
+def DisplayLabelUsingCV2(picture_tensor):
+    picture_tensor = cv2.cvtColor(picture_tensor, cv2.COLOR_RGB2BGR)
+    display_text = '{0:.4f}'.format(0.1234)
+    position = cv2.getTextSize(display_text, cv2.FONT_HERSHEY_DUPLEX, 1, 1)
+    text = dict(width=position[0][0], height=position[0][1], baseline=position[1])
+    cv2.putText(picture_tensor, display_text, (100, 100 - text['baseline']), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0), 2)
+    picture_tensor = cv2.cvtColor(picture_tensor, cv2.COLOR_BGR2RGB)
+
+    # Transpose to Image Type
+    result_image = Image.fromarray(picture_tensor)
+    save_str = 'zzzzzz.png'
+    result_image.save(save_str, 'PNG')
+
+def DisplayLabelUsingPlt():
+    # rect = plt.Rectangle((e[0][0], e[0][1]),
+    #                       80,
+    #                       80, fill=False,
+    #                       edgecolor=(1, 0, 0), linewidth=2.5)
+    # plt.gca().add_patch(rect)
+    # plt.gca().text(e[0][0], e[0][1], 'prob ::  {:.3f}'.format(e[1][0][1]),
+    #                     bbox=dict(facecolor=(1, 0, 0), alpha=0.5), fontsize=9, color='white')
+    return 0
 
 
 if __name__ == '__main__':
