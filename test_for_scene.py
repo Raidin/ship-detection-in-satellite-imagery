82e938e13079392f11ce4428b601c3811e9ee6f5
ContJihunJung <jhjung@contec.kr>
2018-12-24 18:16:07 +0900

    test_for_scene.py 변경  - 함수명 변경  - 주석 추가

--- test_for_scene.py
+++ test_for_scene.py
@@ -5,7 +5,7 @@ from keras.models import model_from_json
 from tqdm import tqdm
 from PIL import Image
 
-def cutting(img, x, y):
+def CropImage(img, x, y):
     area_study = np.arange(3 * 80 * 80).reshape(3, 80, 80)
     for i in range(80):
         for j in range(80):
@@ -23,15 +23,15 @@ def cutting(img, x, y):
     # sys.stdout.write('\rX:{0} Y:{1}  '.format(x, y))
     return area_study
 
-def not_near(x, y, s, coordinates):
+def CheckNearWindow(x, y, s, coordinates):
     result = True
     for e in coordinates:
         if x + s > e[0][0] and x - s < e[0][0] and y + s > e[0][1] and y - s < e[0][1]:
             result = False
     return result
 
-def show_ship(img, x, y, acc, thickness=1):
-    #.Left
+def DisplayBox(img, x, y, acc, thickness=1):
+    # Left
     for i in range(80):
         for th in range(thickness):
             img[0][y + i][x - th] = 255
@@ -99,23 +99,33 @@ def main():
     data_dir = 'data/scenes'
     print os.listdir(data_dir)
 
+    # Test Image list Load
     for idx, img_file in tqdm(enumerate(os.listdir(data_dir))):
         input_image = os.path.join(data_dir, img_file)
         width, height, picture_tensor = ReadImage(input_image, idx)
 
+        # Window Sliding Stride
         step = 10
+        # Predict Information List(X, Y Coordinate and Probability Value)
         coordinates = []
 
+        # Window Sliding Processing Logic
         for y in tqdm(range(int((height - (80 - step)) / step))):
             for x in tqdm(range(int((width - (80 - step)) / step))):
-                area = cutting(picture_tensor, x * step, y * step)
+                area = CropImage(picture_tensor, x * step, y * step)
                 result = model.predict(area)
-                if result[0][1] > 0.90 and not_near(x * step, y * step, 88, coordinates):
+                if result[0][1] > 0.90 and CheckNearWindow(x * step, y * step, 88, coordinates):
                     coordinates.append([[x * step, y * step], result])
-                    print("Probability :: ", result)
+                    print 'Probability :: ', result[0][1]
+                    if 0:
+                        area = np.squeeze(area, axis=0)
+                        plt.imshow(area)
+                        plt.title('Probability :: {}'.format(result[0][1]))
+                        plt.show()
 
+        # display detection box
         for e in coordinates:
-            show_ship(picture_tensor, e[0][0], e[0][1], e[1][0][1])
+            DisplayBox(picture_tensor, e[0][0], e[0][1], e[1][0][1])
 
         # Transpose to Image Type
         picture_tensor = picture_tensor.transpose(1, 2, 0)
