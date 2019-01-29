4db5e5007322cb5efd02e6326fdfb9b05fbca6ca
Raidin <raidin79@gmail.com>
2019-01-29 11:10:11 +0900

    * Modify json_load_practice.py  - modify ground truth dictionary sturcture

--- examples/Manual/json_load_practice.py
+++ examples/Manual/json_load_practice.py
@@ -1,13 +1,18 @@
 import cv2
 import json
 import os
-from matplotlib import pyplot as plt
+
+from collections import namedtuple
+
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 # Ground Truth Structure
 '''
 |--- [Image File Name Dict]
     |--- [Object Type Dict]
-        |--- [Coordinate List Dict]
+        |--- [Coordinate List]
             |--- [xmin, xmax, ymin, ymax]
 '''
 def ParseGT():
@@ -21,16 +26,16 @@ def ParseGT():
             coord_list = []
             obj_dict = dict()
             for coordinate in data['Label'][obj_type]:
+                boxes = []
                 min_value = min(coordinate['geometry'])
                 max_value = max(coordinate['geometry'])
-                coord_dict = {'xmin': min_value['x'],
-                                'ymin': min_value['y'],
-                                'xmax': max_value['x'],
-                                'ymax': max_value['y']}
-                coord_list.append(coord_dict)
+                boxes.append(min_value['x'])  # xmin(0)
+                boxes.append(min_value['y'])  # ymin(1)
+                boxes.append(max_value['x'])  # xmax(2)
+                boxes.append(max_value['y'])  # ymax(3)
+                coord_list.append(boxes)
             obj_dict[obj_type] = coord_list
         ground_truth[data['External ID']] = obj_dict
-
     return ground_truth
 
 def DisplayGT(gt_data):
@@ -39,21 +44,28 @@ def DisplayGT(gt_data):
         img = cv2.imread(os.path.join(scenes_dir, gt))
         b, g, r = cv2.split(img)
         rgb_img = cv2.merge([r, g, b])
-        plt.figure(figsize=(5, 5))
-        plt.imshow(rgb_img)
+
+        fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(6, 6))
+        ax.imshow(rgb_img)
         for obj in gt_data[gt].keys():
-            for coord in gt_data[gt][obj]:
-                width = coord['xmax'] - coord['xmin']
-                height = coord['ymax'] - coord['ymin']
-                rect = plt.Rectangle((coord['xmin'], coord['ymin']), width, height, fill=False, edgecolor=(1, 0, 0), linewidth=1)
+            for boxes in gt_data[gt][obj]:
+                x = boxes[0]
+                y = boxes[1]
+                w = boxes[2] - boxes[0]
+                h = boxes[3] - boxes[1]
+                rect = mpatches.Rectangle((x, y), w, h, fill=False, edgecolor='green', linewidth=1)
+                ax.add_patch(rect)
+                '''
+                rect = plt.Rectangle((boxes[0], boxes[1]), width, height, fill=False, edgecolor=(1, 0, 0), linewidth=1)
                 plt.gca().add_patch(rect)
+                '''
     plt.show()
 
 def main():
+    # detection = namedtuple("Detection", ["image_path", "gt", "pred"])
     gt_data = ParseGT()
-    # DisplayGT(gt_data)
+    DisplayGT(gt_data)
     return
-
 
 if __name__ == '__main__':
     main()
